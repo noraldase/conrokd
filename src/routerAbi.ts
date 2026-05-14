923cd39fee85854d6d9c4cfece7439a3b60d1051
noraldase <norald711@gmail.com>
2026-05-15 00:42:22 +0700

    Create routerAbi.ts

---
+++ src/routerAbi.ts
@@ -0,0 +1,4 @@
+export const ROUTER_ABI = [
+  'function swapExactETHForTokensSupportingFeeOnTransferTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable',
+  'function swapExactTokensForETHSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external'
+]
